f4e462de28c07e3092ec1687fcba874f6ea7184f
Zhang RD <zhangr.d.1996@gmail.com>
2017-12-14 19:39:20 +0800

    changed structure

--- UI/js/main.js
+++ UI/js/main.js
@@ -15,7 +15,64 @@ for (var i = 0; i < 4; i++) {
 var states = {
     preload: function () {
         this.preload = function () {
+            game.stage.backgroundColor = '#ddd';
 
+            game.load.crossOrigin = 'anonymous';
+            for (var i = 0; i < cells.length(); i++) {
+                game.load.image(cells[i].img, '.img/' + cells[i].img);
+            }
+
+            var progressText = game.add.text(game.world.centerX, game.world.centerY, '0%', {
+                fontSize: '20px',
+                fill: '#555'
+            });
+            progressText.anchor.setTo(0.5, 0.5);
+            game.load.onFileComplete.add(function (progress) {
+                progressText.text = progress + '%';
+            });
+            game.load.onLoadComplete.add(onLoad);
+            var ddl = false;
+            setTimeout(function () {
+                ddl = true;
+            }, 100);
+
+            function onLoad() {
+                if (ddl) {
+                    game.stage.start('created');
+                } else {
+                    setTimeout(onLoad, 1000);
+                }
+            }
+        }
+    },
+
+    created: function () {
+        this.create = function () {
+
+            var title = game.add.text(game.world.centerX, game.world.height * 0.4, 'FC15', {
+                fontSize: '40px',
+                fontWeight: 'bold',
+                fill: '#222'
+            });
+            title.anchor.setTo(0.5, 0.5);
+
+            var viewButton = game.add.text(game, game.world.centerX, game.world.height * 0.7, 'View', {
+                fontSize: '40px',
+                fill: '#111',
+                backgroundColor: '#bbb',
+                //                height: game.world.height * 0.1,
+                //                width: game.world.width * 0.2
+            });
+            viewButton.anchor.setTo(0.5, 0.5);
+            viewButton.input.onTap.add(function () {
+                viewButton.backgroundColor = '#999';
+                game.state.start('view');
+            });
+        }
+    },
+
+    view: function () {
+        this.create = function () {
             $.getJSON("js/LogSchema.json", function(data) {
                 totalRounds = data.head.totalRounds;
                 totalPlayers = data.head.totalPlayers;
@@ -100,67 +157,6 @@ var states = {
                     });
                 });
             });
-            game.stage.backgroundColor = '#ddd';
-
-            game.load.crossOrigin = 'anonymous';
-            for (var i = 0; i < cells.length(); i++) {
-                game.load.image(cells[i].img, '.img/' + cells[i].img);
-            }
-
-            var progressText = game.add.text(game.world.centerX, game.world.centerY, '0%', {
-                fontSize: '20px',
-                fill: '#555'
-            });
-            progressText.anchor.setTo(0.5, 0.5);
-            game.load.onFileComplete.add(function (progress) {
-                progressText.text = progress + '%';
-            });
-            game.load.onLoadComplete.add(onLoad);
-            var ddl = false;
-            setTimeout(function () {
-                ddl = true;
-            }, 100);
-
-            function onLoad() {
-                if (ddl) {
-                    game.stage.start('created');
-                } else {
-                    setTimeout(onLoad, 1000);
-                }
-            }
-        }
-    },
-
-    created: function () {
-        this.create = function () {
-
-            var title = game.add.text(game.world.centerX, game.world.height * 0.4, 'FC15', {
-                fontSize: '40px',
-                fontWeight: 'bold',
-                fill: '#222'
-            });
-            title.anchor.setTo(0.5, 0.5);
-
-            var viewButton = game.add.text(game, game.world.centerX, game.world.height * 0.7, 'View', {
-                fontSize: '40px',
-                fill: '#111',
-                backgroundColor: '#bbb',
-                //                height: game.world.height * 0.1,
-                //                width: game.world.width * 0.2
-            });
-            viewButton.anchor.setTo(0.5, 0.5);
-            viewButton.input.onTap.add(function () {
-                viewButton.backgroundColor = '#999';
-                game.state.start('view');
-            });
-        }
-    },
-
-    view: function () {
-        this.create = function () {
-            for (var i = 0; i < cells.length; i++) {
-                cells[i].draw();
-            }
         }
     },
 
